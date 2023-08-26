8f39042058f5b0dace2068568342d4f6be06628e
LeasiFrancesca <38890387+LeasiFrancesca@users.noreply.github.com>
2023-08-26 12:25:11 -0400

    nemertea-microbiome.R

--- nemertea-microbiome.R
+++ nemertea-microbiome.R
@@ -3,11 +3,12 @@
 library(ape)
 library(car)
 library(ecodist)
+library(geosphere)
 library(ggplot2)
 library(ggsci)
+library(marmap)
 library(topicmodels)
 library(vegan)
-
 
 ##### load data anche check it #####
 
@@ -53,7 +54,6 @@ OTUs_ord <- OTUs[match(rownames(DNA_dist), OTUs$species),]
 all(rownames(DNA_dist)==OTUs_ord$species)
 which(rownames(DNA_dist)!=OTUs_ord$species)
 
-
 # obtain matrix of Bray-Curtis distances, ordered as in the matrix of phylogenetic distances
 
 OTUs_ord_BC <- as.matrix(ecodist::bcdist(as.matrix(OTUs_ord[,-1])))
@@ -67,7 +67,6 @@ plot(OTUs_ord_BC, DNA_dist)
 
 plot(as.vector(DNA_dist),as.vector(1-OTUs_ord_BC), xlab="phylogenetic distance", ylab="community similarity")
 plot(vegan::mantel.correlog((OTUs_ord_BC), DNA_dist, cutoff=F, n.class=7))
-
 
 # tanglegram
 
@@ -89,10 +88,9 @@ write.tree(t1,"nemertea_18s_ULTRA.tree")
 
 plot(hclust(as.dist(OTUs_dist_BC), method="average"), labels=OTUs$species, hang=-1)
 
-
 ##### effect of host genus or geography #####
 
-nemert <- read.csv("nemertea_taxonomy.csv", header=T, as.is=F)
+nemert <- read.csv("nemertea_taxonomy 230812.csv", header=T, as.is=F)
 
 nemert_reduced <- droplevels(nemert[nemert$rank!="none",])
 OTUs_reduced <- droplevels(OTUs[nemert$rank!="none",])
@@ -106,9 +104,6 @@ OTUs_dist_reduced <- ecodist::bcdist(as.matrix(OTUs_reduced[,-1]))
 
 effects <- vegan::adonis2(as.matrix(OTUs_dist_reduced) ~ nemert_reduced$Genus + nemert_reduced$Locality)
 effects
-
-
-
 
 ##### analyses on only the genus Ototyphlonemertes #####
 
@@ -128,7 +123,6 @@ which(nemert$SampleID!=OTUs$species)
 oto <- na.omit(droplevels(nemert[nemert$Genus=="Ototyphlonemertes",]))
 names(oto)
 dim(oto)
-
 
 oto_m <- OTUs[OTUs$species %in% levels(oto$SampleID),]
 dim(oto_m)
@@ -163,18 +157,16 @@ oto_m2_dist <- ecodist::bcdist(as.matrix(oto_m2[,-1]))
 effects_oto <- vegan::adonis2(as.matrix(oto_m2_dist) ~ oto$taxonomy + oto$Locality)
 effects_oto
 
-
-
 ##### correlation phylogeny-microbiome #####
 
 DNA_dist <- ape::cophenetic.phylo(tree55)
 dim(DNA_dist)
 
-oto_33 <- droplevels(oto_m$species)
+oto_34 <- droplevels(oto_m$species)
 
-DNA_dist_oto1 <- DNA_dist[rownames(DNA_dist) %in% oto_33,]
+DNA_dist_oto1 <- DNA_dist[rownames(DNA_dist) %in% oto_34,]
 DNA_dist_oto2 <- t(DNA_dist_oto1)
-DNA_dist_oto <- DNA_dist_oto2[rownames(DNA_dist_oto2) %in% oto_33,]
+DNA_dist_oto <- DNA_dist_oto2[rownames(DNA_dist_oto2) %in% oto_34,]
 
 dim(DNA_dist_oto)
 
@@ -205,6 +197,35 @@ plot(oto_m_ord_BC, DNA_dist_oto)
 
 plot(as.vector(DNA_dist_oto),as.vector(1-oto_m_ord_BC), xlab="phylogenetic distance", ylab="community similarity")
 plot(vegan::mantel.correlog((oto_m_ord_BC), DNA_dist_oto, cutoff=F, n.class=7))
+
+##### correlation phylogeny-microbiome checking for geography ######
+
+# check that names of the samples are ordered in the same way
+all(rownames(DNA_dist_oto)==oto$sampleID)
+which(rownames(DNA_dist_oto)!=oto$sampleID)
+all(oto_m_ord$species==oto$sampleID)
+which(oto_m_ord$species!=oto$sampleID)
+
+# map of the sampling area
+centrAmer <- marmap::getNOAA.bathy(lon1=-98,lon2=-72, lat1=4,lat2=22.9,resolution=1)  
+
+# check that dephts make sense
+depths <- get.depth(centrAmer,oto$longitude, oto$latitude, locator = F)
+depths$depth > -1 ## all good
+rm(depths)
+
+# Euclidean distances between beaches  
+d.euclidean <- as.matrix(geodist::geodist(oto[c("longitude", "latitude")], measure = "geodesic"))
+
+oto_geogr_dist <- geosphere::distm(cbind(oto$longitude, oto$latitude), fun=distGeo) # an alternative
+
+# Least cost distance between beaches, considering 1,100 m depth
+dist.lc <- marmap::lc.dist(trans.mat(centrAmer, min.depth=-1,  max.depth=-200), oto[c("longitude","latitude")], res="dist") 
+
+# partial Mantel tests
+vegan::mantel.partial(oto_m_ord_BC, DNA_dist_oto, d.euclidean)
+vegan::mantel.partial(oto_m_ord_BC, DNA_dist_oto, oto_geogr_dist) # alternative
+vegan::mantel.partial(oto_m_ord_BC, DNA_dist_oto, dist.lc)
 
 
 
